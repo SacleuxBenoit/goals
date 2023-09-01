36f57fed3dd67c0ada2f195e6188d94d44156f8f
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-09-01 20:26:42 +0200

    create routes && controllers file with CRUD for goals

---
+++ backend/controllers/goalControllers.js
@@ -0,0 +1,30 @@
+// @desc Get goals
+// @route Get /api/goals
+const getGoals = (req,res) => {
+    res.status(200).json({message : "Get goals"});
+}
+
+// @desc POST goal
+// @route POST /api/goals
+const setGoal = (req,res) => {
+    res.status(200).json({message: "Create goal"})
+}
+
+// @desc UPDATE goal
+// @route UPDATE /api/goals/:id
+const updateGoal = (req,res) => {
+    res.status(200).json({message: `Update goal ${req.params.id}`})
+}
+
+// @desc DELETE goal
+// @route DELETE /api/goals/:id
+const deleteGoal = (req,res) => {
+    res.status(200).json({message: `Delete goal ${req.params.id}`})
+}
+
+module.exports = {
+    getGoals,
+    setGoal,
+    updateGoal,
+    deleteGoal
+}
