ab3025aa0b63a707ff1f376dd1e1ce562c881b4c
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-09-01 22:08:40 +0200

    add Error && Exception handling

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -7,6 +7,10 @@ const getGoals = (req,res) => {
 // @desc POST goal
 // @route POST /api/goals
 const setGoal = (req,res) => {
+    if(!req.body.text){
+        res.status(400)
+        throw new Error('Please add a text field')
+    }
     res.status(200).json({message: "Create goal"})
 }
 
