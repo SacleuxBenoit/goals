af9cf6e170e42886b44660f9971b5033f88599d5
SacleuxBenoit <benoitsacleux@outlook.fr>
2023-09-04 14:17:26 +0200

    add async handler

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,23 +1,24 @@
+const asyncHandler = require('express-async-handler');
 // @desc Register new user
 // @route POST /api/users
 // @access Public
-const registerUser = (req,res) => {
+const registerUser = asyncHandler(async (req,res) => {
     res.json({message: "user register"})
-}
+})
 
 // @desc Authenticate a user
 // @route POST /api/users/login
 // @access Public
-const loginUser = (req,res) => {
+const loginUser = asyncHandler(async (req,res) => {
     res.json({message: "user login"})
-}
+})
 
 // @desc Get user
 // @route GET /api/users/me
 // @access Public
-const getMe = (req,res) => {
+const getMe = asyncHandler(async (req,res) => {
     res.json({message: "user data display"})
-}
+})
 
 module.exports = {
     registerUser,
